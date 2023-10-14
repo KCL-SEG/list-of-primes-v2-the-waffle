46e81b894a28f72c7a9ea05e7c7ff2f284555194
the-waffle <43791700+the-waffle@users.noreply.github.com>
2023-10-14 23:12:50 +0100

    Write prime finding algorithm

--- primes.py
+++ primes.py
@@ -1,6 +1,33 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def primes(number_of_primes):
-    list = []
-    return list
+import math
+
+def is_prime(x : int) -> bool:
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+def primes(number_of_primes : int) -> list[int]:
+    if number_of_primes <= 0:
+        raise ValueError()
+
+    if number_of_primes == 1:
+        return [2]
+    if number_of_primes == 2:
+        return [2,3]
+    
+    ret : list[int] = [2, 3]
+    n = 1
+    while len(ret) < number_of_primes:
+        if is_prime(6 * n - 1):
+            ret.append(6 * n - 1)
+        if is_prime(6 * n + 1):
+            ret.append(6 * n + 1)
+        n += 1
+
+    if len(ret) == number_of_primes + 1:
+        ret.pop()
+
+    return ret
